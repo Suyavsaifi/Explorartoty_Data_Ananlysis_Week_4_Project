7e5398b470e193070af13123fafeb766b76c5a92
Suyav Saifi <39773803+Suyavsaifi@users.noreply.github.com>
2018-12-17 07:22:07 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,15 @@
 ## read in emissions data and classification code
-emissions_data <- readRDS("summarySCC_PM25.rds")
+emissions_data <- readRDS("C:/Users/DELL/Documents/internet_data/Coursera_Exploratory_Data_Analysis_Week-4_Project/summarySCC_PM25.rds")
 class_code <- readRDS("Source_Classification_Code.rds")
 
+## creating a copy of the data
+
+copy_emissions_data<-emission_data
+
+## working on a copy of that data
+
 ## add up the total emissions for each year
-sum_by_year <- aggregate(emissions_data$Emissions, by=list(year=emissions_data$year), FUN=sum)
+sum_by_year <- aggregate(copy_emissions_data$Emissions, by=list(year=copy_emissions_data$year), FUN=sum)
 
 ## create the plot
 png(filename = "plot1.png")
@@ -12,4 +18,3 @@ plot(sum_by_year$year, sum_by_year$x, type = "l",
      ylab = "Total emissions of PM2.5 (tons)",
      xlab = "Year")
 dev.off()
-
